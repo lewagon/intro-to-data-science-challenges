991c2a45fd1c69be80092add7b7303efc1b5b73e
Gaëtan Manchon <gmanchon@gmail.com>
2023-10-10 16:41:48 +0200

    add requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = [c.strip() for c in f.readlines()]
+
+setup(
+    name='WagonIntroToDS',
+    version='1.1',
+    install_requires=requirements,
+    description='Wagon Intro to Data Science'
+)
